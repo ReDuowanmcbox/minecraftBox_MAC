992bf203c30b3f05e58d3ec93153c0be26aeedef
aLoongQwQ <moe@mc-m.net>
2023-02-06 15:14:02 +0800

    minecraft news

--- i.py
+++ i.py
@@ -1,9 +1,14 @@
 import flet as ft
 import webbrowser
+import requests
 import random
 import os
 
 def main(page):
+    try:
+        news = requests.get("http://low-webing-newsapi.mc-m.net/")
+    except:
+        news = '网络错误'
     def btn_click(e):
         if not txt_name.value:
             txt_name.error_text = "请输入您的启动器路径"
@@ -18,4 +23,6 @@ def main(page):
 
     page.add(txt_name, ft.ElevatedButton("启动游戏", on_click=btn_click))
     page.add(txt_name, ft.ElevatedButton("我的世界:故事模式", on_click=goStory))
+    t = ft.Text(value=str(news), color="green")
+    page.controls.append(t)
 ft.app(target=main,port=random.randint(8255,9000))
